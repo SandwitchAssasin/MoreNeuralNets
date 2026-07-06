1f0cb1dd6675dad14e723dc70385b13a50a12051
WoodOld <87334785+SandwitchAssasin@users.noreply.github.com>
2026-07-06 16:08:03 +0200

    Add files via upload

---
+++ NeurNetsFromBeginNUMPY.py
@@ -0,0 +1,212 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+#BOTH FUNCTIONS ONLY FOR VERTICAL MATRICES
+def Sigmoid(X):
+    S = np.zeros(shape=X.shape)
+    for i in range(0,len(X)):
+        x = X[i][0]
+        S[i] = 1/(1+math.exp(-x))
+    return S
+def SigmoidDer(X):
+    S = np.zeros(shape=X.shape)
+    S = Sigmoid(X)*(1-Sigmoid(X))
+    return S 
+def ReLU(X):
+    S = np.zeros(shape=X.shape)
+    for i in range(0,len(X)):
+        x = X[i][0]
+        if x <= 0:
+            S[i] = 0
+        else:
+            S[i] = x
+    return S 
+def ReLUDer(X):
+    S = np.zeros(shape=X.shape)
+    for i in range(0,len(X)):
+        x = X[i][0]
+        if x <= 0:
+            S[i] = 0
+        else:
+            S[i] = 1
+    return S 
+def LeakyReLU(X, alpha):
+    S = np.zeros(shape=X.shape)
+    for i in range(0,len(X)):
+        x = X[i][0]
+        if x <= 0:
+            S[i] = alpha * x
+        else:
+            S[i] = x
+    return S 
+def LeakyReLUDer(X, alpha):
+    S = np.zeros(shape=X.shape)
+    for i in range(0,len(X)):
+        x = X[i][0]
+        if x <= 0:
+            S[i] = alpha
+        else:
+            S[i] = 1
+    return S 
+def Softmax(X):
+    S = np.zeros(shape=X.shape)
+    c = 0
+    for i in range(0,len(X)):
+        x = X[i][0]
+        c = c + math.exp(x)
+    for i in range(0,len(X)):
+        x = X[i][0]
+        S[i] = math.exp(x)/c
+   # print(X, ':', S)
+    return S 
+def SoftmaxDer(X):
+    S = np.zeros(shape=X.shape)
+    S = Softmax(X)*(np.ones(shape=X.shape)- Softmax(X))
+    return S 
+class DenseLayer:
+    def __init__(self, input_size, output_size, activation, optional_alpha = 0):
+        self.input_size = input_size
+        self.output_size = output_size
+        self.weights = np.random.randn(output_size,input_size)
+        self.biases = np.random.randn(output_size,1)
+        self.activation = activation
+        self.optional_alpha = optional_alpha
+    def Forward(self, inputs):
+        self.remInputs = inputs
+        '''
+        if self.activation == 'relu' or self.activation == 'leaky_relu':
+            mean = np.sum(inputs)/len(inputs)
+            sd = max(np.std(inputs),1)
+            inputs = (inputs - mean)/sd
+            '''
+        self.S = self.weights @ inputs + self.biases
+        match self.activation:
+            case 'sigmoid':
+                self.Y = Sigmoid(self.S)
+            case 'relu':
+                self.Y = ReLU(self.S)
+            case 'leaky_relu':
+                self.Y = LeakyReLU(self.S, self.optional_alpha)
+            case 'softmax':
+                self.Y = Softmax(self.S)
+        return self.Y
+    def Backward(self, last_delta, last_weights, is_last_layer):
+        if is_last_layer:
+            match self.activation:
+                case 'sigmoid':
+                    derv = SigmoidDer(self.S)
+                case 'relu':
+                    derv = ReLUDer(self.S)
+                case 'leaky_relu':
+                    derv = LeakyReLUDer(self.S, self.optional_alpha)
+                case 'softmax':
+                    derv = SoftmaxDer(self.S)
+            self.delta = derv * last_delta
+        else:
+            match self.activation:
+                case 'sigmoid':
+                    derv = SigmoidDer(self.S)
+                case 'relu':
+                    derv = ReLUDer(self.S)
+                    
+                case 'leaky_relu':
+                    derv = LeakyReLUDer(self.S, self.optional_alpha)
+                case 'softmax':
+                    derv = SoftmaxDer(self.S)
+            wage_l_delta_prod = last_weights.T @ last_delta
+            self.delta = derv * wage_l_delta_prod
+            #print(self.delta)
+    def Learn(self, learning_rate):
+        if self.delta is None:
+            raise Exception("THERE IS NO DELTA! Throw Backward before Learn")
+        else:
+            deltaWeight = self.remInputs @ self.delta.T
+            wT = self.weights.T - learning_rate*deltaWeight
+            self.weights = wT.T
+            self.biases = self.biases - learning_rate*self.delta
+class LayerNormalization:
+    def __init__(self, input_size, output_size, activation, optional_alpha = 0):
+        self.input_size = input_size
+        self.output_size = output_size
+        if output_size != input_size:
+            raise Exception('Output size must be the same as Input size in LayerNormalization layer!')
+        self.gammas = np.random.randn(input_size,1)
+        self.biases = np.random.randn(input_size,1)
+    def Forward(self, inputs):
+        self.remInputs = inputs
+        self.mean = np.sum(inputs)/self.input_size
+        self.var = ((inputs - self.mean)*(inputs - self.mean))/self.input_size
+        self.epsilon = 0.00001
+        self.outputs = (inputs - self.mean)/math.sqrt(self.var + self.epsilon)
+       
+        return self.outputs
+    def Backward(self, last_delta, last_weights):
+        gamma_squared_vec = self.gammas*(self.remInputs-self.mean)*(self.remInputs-self.mean)
+        other_side_of_der = (1 - 1/self.input_size)*(2/self.input_size)*(-1/(2*(math.sqrt(self.var + self.epsilon)**3)))
+        y_on_x_der = gamma_squared_vec * other_side_of_der
+        self.delta = last_delta * y_on_x_der
+        #TUTAJ SKONCZYLEM
+    def Learn(self, learning_rate):
+        if self.delta is None:
+            raise Exception("THERE IS NO DELTA! Throw Backward before Learn")
+        else:
+            deltaWeight = self.remInputs @ self.delta.T
+            wT = self.weights.T - learning_rate*deltaWeight
+            self.weights = wT.T
+            self.biases = self.biases - learning_rate*self.delta
+class Model:
+    def __init__(self, layers):
+        self.input_size = layers[0].input_size
+        self.size = len(layers)
+        self.layers = layers
+        self.error = 0
+    def Forward(self, inputs):
+        '''Inputs->outputs'''
+        input_data = np.expand_dims(np.array(inputs),0)
+        if len(input_data[0]) != self.input_size:
+            raise Exception("Wrong input size!")
+        x = input_data.T
+        for i in range(0, self.size):
+            x = self.layers[i].Forward(x) 
+        return x
+    def Backward(self, inputs, real_values):
+        '''Calculating deltas and error'''
+        #first delta - the 1D matrix containing all first derivtives of target function via model-output values
+        predictions = self.Forward(inputs)
+    
+        #Error-----------------------------------
+
+        #SquaredError
+        #er_delta = 2*(predictions - real_values.T) 
+        
+        self.error = math.sqrt(np.sum((predictions - real_values.T)*(predictions - real_values.T)))
+
+        #Categorical cross-entropy WITH SOFTMAX ON LAST LAYER
+        if(self.layers[-1].activation != 'softmax'):
+            raise Exception('You are using cat. cross-entropy with no softmax on the last layer. USE SOFTMAX PLEASE')
+        er_delta = predictions - real_values.T
+
+        self.layers[self.size-1].Backward(er_delta, None, is_last_layer = True)
+        delta = self.layers[self.size-1].delta
+        for i in range(self.size-2, -1,-1):
+            self.layers[i].Backward(delta, self.layers[i+1].weights, is_last_layer = False)
+            delta = self.layers[i].delta
+    def Learn(self, learning_rate):
+        '''Using deltas to learn the net'''
+        for i in range(0, self.size):
+            self.layers[i].Learn(learning_rate)
+    def Train(self, data_base, epochs, learning_rate):
+        '''Actual train function'''
+        for i in range(epochs):
+            cum_error = 0
+            for data in data_base:
+                input_data = data[0]
+                output_data = np.expand_dims(np.array(data[1]),0)
+                self.Backward(input_data,output_data)
+                self.Learn(learning_rate)
+                cum_error = cum_error + self.error
+            cum_error = cum_error / len(data_base)
+            print('Epoch nr. ' + str(i) + ' Error: ' + str(round(cum_error,4)))
+
+
+#Sigmoid wiec wyjscia sa od [0,1]
---
+++ main.py
@@ -0,0 +1,95 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import NeurNetsFromBeginNUMPY as NeurNFN
+'''
+m = NeurNFN.Model(2, (4,6,1))
+print(m.Forward((4,2)))
+databs = [[(4,2),1],[(1,1),1],[(1.5,1.6),1],[(1.2,1.4),1],
+          [(3.4,4),0],[(0.3,0.5),0],[(1.9,0.1),0],[(0.2,4.4),0],[(1.5,3.4),0],
+          [(4,1.4),0],[(4,3),0]]
+for dt in databs:
+    dt[1] = [dt[1]]
+
+m.Train(databs, 4000, 0.03)
+plt.subplot(1,2,1)
+for data in databs:
+    colr = 'blue'
+    if(data[1][0] == 1):
+        colr = 'red'
+    plt.plot(data[0][0],data[0][1],marker='o',color=colr)
+plt.subplot(1,2,2)
+X = np.linspace(-1,5,1000)
+Y = np.random.uniform(0,4.5,1000)
+c = []
+for cint in range(0,1000):
+    c.append(m.Forward((X[cint],Y[cint])))
+
+print(m.Forward((4,2)))
+plt.scatter(X,Y,c=c,cmap='coolwarm')
+plt.show()
+'''
+
+#Real project - Irises
+
+def BladPredykcji(pred, real):
+    suma = 0
+    for i in range(0,len(pred)):
+        suma = suma + (pred[i] - real[i])**2
+    return suma/len(pred)
+
+with open('iris.txt') as f:
+    irises = {'Iris-setosa':0,'Iris-versicolor':1,'Iris-virginica':2}
+    irises_res = {0:'Iris-setosa',1:'Iris-versicolor',2:'Iris-virginica'}
+    data =[]
+
+    lines = f.readlines()
+    for line in lines:
+        boxes = line.rstrip('\n').split(',')
+        floatline = []
+        dataline = []
+
+        floatline.append((float)(boxes[0])/10)
+        floatline.append((float)(boxes[1])/10)
+        floatline.append((float)(boxes[2])/10)
+        floatline.append((float)(boxes[3])/10)
+        dataline.append(tuple(floatline))
+        irisL = [0,0,0]
+        mI = irises[boxes[4]]
+        irisL[mI] = 1
+        dataline.append(irisL)
+        data.append(dataline)
+print(data)
+dataTrain = []
+dataTest = []
+for d in data:
+    r = np.random.uniform(0,1)
+    if r < 0.8:
+        dataTrain.append(d)
+    else:
+        dataTest.append(d)
+l1 = NeurNFN.DenseLayer(4,10,'leaky_relu', 0.3)
+l2 = NeurNFN.DenseLayer(10,20,'leaky_relu', 0.3)
+l3 = NeurNFN.DenseLayer(20,10,'leaky_relu', 0.3)
+l4 = NeurNFN.DenseLayer(10,3,'softmax')
+lay = [l1,l2,l3,l4]
+m = NeurNFN.Model(lay)
+m.Train(dataTrain,epochs = 300, learning_rate = 0.006)
+'''
+l1_2 = NeurNFN.DenseLayer(4,10,'sigmoid')
+l2_2 = NeurNFN.DenseLayer(10,20,'sigmoid')
+l3_2 = NeurNFN.DenseLayer(20,10,'sigmoid')
+l4_2 = NeurNFN.DenseLayer(10,3,'sigmoid')
+lay = [l1_2,l2_2,l3_2,l4_2]
+m2 = NeurNFN.Model(lay)
+
+m2.Train(dataTrain,epochs = 5, learning_rate = 0.03)'''
+for d in dataTest:
+    predictions = m.Forward(d[0])
+    real = d[1]
+    print(irises_res[np.argmax(real)], ":", irises_res[np.argmax(predictions)], ":" , round(BladPredykcji(predictions,real)[0],2))
+'''for d in dataTest:
+    predictions = m2.Forward(d[0])
+    real = d[1]
+    print(irises_res[np.argmax(real)], ":", irises_res[np.argmax(predictions)], ":" , round(BladPredykcji(predictions,real)[0],2))
+   '''    
